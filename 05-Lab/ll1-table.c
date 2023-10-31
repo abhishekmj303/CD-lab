7fdf806ff590d3b4716ea2332c6e8137be78efc4
Abhishek M J <abhishekmj303@gmail.com>
2023-10-31 14:49:29 +0530

    Add CFG.txt

--- 05-Lab/ll1-table.c
+++ 05-Lab/ll1-table.c
@@ -104,11 +104,11 @@ void ll1_table(CFG cfg)
 
 int main(int argc, char *argv[])
 {
-    // if (argc < 2) {
-    //     printf("Usage: %s <CFG-input-file>\n", argv[0]);
-    //     return 1;
-    // }
-    argv[1] = "CFG";
+    if (argc < 2) {
+        printf("Usage: %s <CFG-input-file>\n", argv[0]);
+        return 1;
+    }
+    // argv[1] = "CFG";
 
     FILE *fp = fopen(argv[1], "r");
     if (fp == NULL) {
